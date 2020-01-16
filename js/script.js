54a0bd112b7690cb95ebffb2cd62b0aff5196d68
Vicillian <vic.liu101@gmail.com>
2020-01-16 02:22:02 +0000

    I've added the song name button and an Input to put your songs in.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-let songs = [];
+let songs = [$(".songName").val()];
+let songs2 = ["Take me Road","Old Town Home",];
+$(".nameButton").click(function() {
+  songs2.push("triangle");
+  console.log(songs2);
+});
